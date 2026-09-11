f9f408c19b51abd321e92cab61e3c96a90b77a6e
sumayi07 <max.ysun07@gmail.com>
2026-09-11 01:15:01 -0400

    add day 15

---
+++ day_15/error_types.py
@@ -0,0 +1,34 @@
+# Day 15
+
+# print "hello world"
+# print("hello world")
+
+# print(age)
+
+# age = 25
+# print(age)
+
+# numbers = [1, 2, 3]
+# print(numbers[3])
+# print(numbers[1])
+
+# import maths
+# import math
+
+# math.PI
+# math.pi
+
+# users = {'name':'Asab', 'age':250, 'country':'Finland'}
+# print(users["name"])
+# print(users["county"])
+
+# 4 + "3"
+# 4 + 3
+
+# from math import power
+# from math import pow
+
+# int("12a")
+# int("12")
+
+# 1/0
