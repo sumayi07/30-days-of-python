4cdcd532f04f0fcefdf044983c3d87aa8d86fd0e
sumayi07 <max.ysun07@gmail.com>
2026-09-21 15:11:13 -0700

    day 28

---
+++ day_28/api.py
@@ -0,0 +1,11 @@
+# Day 28
+
+# the assignment today was to just read their post about apis and http so I'll summarize what I learned here
+
+# APIs are interfaces that allow interactions between an enterprise and an application that uses it's assets.
+# In other words, an API is what allows two software applications to communicate and share data with each other
+
+# HTTP is a protocol of sharing data that is commonly used by APIs.
+# It is a client-server protocol. The client (a browser) opens a connection with the server (a web server) and requests data.
+# The server then returns the data that is requested and closes the connection.
+# The client can make four different types of data requests. GET (used to read data), POST (used to create data). PUT (used to update data), DELETE (used to delete data)
